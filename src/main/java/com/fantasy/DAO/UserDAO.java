306e5208d0f2d1e47c0ae5d5723d2d08617595c6
Lucas Oliveira <serg.lucas.oliveira@gmail.com>
2016-06-22 15:40:14 +0100

    merged

--- src/main/java/com/fantasy/DAO/UserDAO.java
+++ src/main/java/com/fantasy/DAO/UserDAO.java
@@ -6,11 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserDAO extends CrudRepository<User, Long> {
 
     User findByUsername(String username);
-<<<<<<< HEAD
-=======
     User findByUsernameOrEmail (String username, String email);
- //   User findOne(long id);
- //   User save(User user);
- //   Iterable<User> findAll();
->>>>>>> validacoes-feitas
 }
